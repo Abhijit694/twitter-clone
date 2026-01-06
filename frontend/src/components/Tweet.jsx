9fe3df487728fe38c498a4f59fe054be3c8f12dc
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-06 16:54:06 +0530

    update username layout

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -1,9 +1,11 @@
-
 import { FaRegComment } from "react-icons/fa";
 import { BiRepost } from "react-icons/bi";
 import { IoHeartOutline,IoHeart } from "react-icons/io5";
 import { PiBookmarkSimple,PiBookmarkSimpleBold } from "react-icons/pi";
 import Avatar from "react-avatar"
+import { PiDotLight } from "react-icons/pi";
+import { PiDotOutlineFill } from "react-icons/pi";
+import { LuDot } from "react-icons/lu";
 
 
 const Tweet = () => {
@@ -11,8 +13,16 @@ const Tweet = () => {
     <>
         <div className="w-full p-3 flex gap-3">
             <Avatar googleId="118096717852922241760" size="40" name="Abhijit Nayak" round={true} />
-            <div className="w-full flex flex-col text-[22px] text-gray-500">
-                <span>This is a sample text</span>
+            <div className="w-full flex flex-col text-[17px]">
+                <div className="flex items-center gap-1">
+                  <div className="font-bold">name</div>
+                  <div className="flex items-center">
+                    <div className="text-gray-600">@username</div>
+                    <LuDot/>
+                    <div className="text-gray-600">upload-time</div>
+                  </div>
+                </div>
+                <div>This is a sample text</div>
                 {/* if image available shown here */}
                 ------------------------------------
                 {/* Like share comment */}
