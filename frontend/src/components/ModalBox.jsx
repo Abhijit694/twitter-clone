15a625f624a4852b67693ff8184e3cbcd70527be
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-15 10:03:24 +0530

    visual changes

--- frontend/src/components/ModalBox.jsx
+++ frontend/src/components/ModalBox.jsx
@@ -5,10 +5,10 @@ const ModalBox = ({isOpen,onClose,children}) => {
     if(!isOpen) return null
 
     return ( 
-        <div className="fixed inset-0 bg-gray-200/30 backdrop-blur-sm flex justify-center items-center z-10"
+        <div className="fixed inset-0 bg-gray-600/30 backdrop-blur-sm flex justify-center items-center z-10"
         // onClick={onClose}
         >
-            <div className="bg-white p-6 rounded-4xl shadow-xl w-[48%] z-20">
+            <div className="h-150 bg-white p-6 rounded-4xl shadow-xl w-[48%] z-20">
                 <div className="flex items-center mb-4">
                     <div className="w-[45%]">
                         <div onClick={onClose} className="size-8 flex justify-center items-center rounded-full text-gray-400 hover:cursor-pointer hover:bg-gray-100 hover:text-black"><RxCross2 size='18px'/></div>
