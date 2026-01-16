9332f185d224c0423b79d28afe5623672c67c3b2
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-16 19:16:36 +0530

    feat: new following and followers field added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,6 +19,14 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true
+        },
+        followers: {
+            type: Array,
+            default: []
+        },
+        following: {
+            type: Array,
+            default: []
         }
     },
     { timestamps:true }
