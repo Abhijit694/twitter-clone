af47398d493f4a4daadd80c62fc254fdc1b71d11
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-16 16:54:12 +0530

    feat: initialize database connection in server entry point

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import express from "express"
+import connectDB from "./config/database.js"
 
 dotenv.config({
     path:".env"
@@ -7,8 +8,12 @@ dotenv.config({
 
 const app = express()
 
+// Connect to database
+connectDB()
 
 
+
+// Start server
 app.listen(process.env.PORT,() => {
     console.log(`Server listening on port ${process.env.PORT}`)
 })
