bc861dfa18613f012a7ae8ba234a8c3ef622a4bf
Abhijit694 <nayakabhijit543@gmail.com>
2026-01-16 17:08:20 +0530

    feat: user model added

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps:true }
+)
+
+export default User = mongoose.model("User",userSchema);
