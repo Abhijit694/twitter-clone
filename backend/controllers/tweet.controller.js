d888b0458849b63d4d8600c9c5cb35af690944ee
Abhijit694 <nayakabhijit543@gmail.com>
2026-02-03 12:44:30 +0530

    feat: delete tweet functionality implemented

--- backend/controllers/tweet.controller.js
+++ backend/controllers/tweet.controller.js
@@ -19,10 +19,44 @@ export const createTweet = async (req,res) => {
         return res.status(201).json({
             message: "Tweet created successfully",
             success: true,
-            tweet: newTweet
         })
     } catch (error) {
         console.log("Error creating tweet : ",error)
+        return res.status(500).json({
+            message: "Internal server error",
+            success: false
+        })
+    }
+}
+
+export const deleteTweet = async (req,res) => {
+    try {
+        const {id} = req.params
+        const loggedInUserId = req.id   // This is the id of user trying to delete
+        // 1. Find the tweet to ensure it exists and get its owner
+        const tweet = await Tweet.findById(id)
+        if(!tweet){
+            return res.status(404).json({
+                message: "Tweet not found",
+                success: false
+            })
+        }
+        // 2. Check if the logged-in user is the owner of the tweet
+        if(tweet.userId.toString() !== loggedInUserId){
+            // 403 Forbidden
+            return res.status(403).json({
+                message: "You are not authorized to delete this tweet",
+                success: false
+            })
+        }
+        // 3. If authorized then proceed with deletion
+        await Tweet.findByIdAndDelete(id)
+        return res.status(200).json({
+            message: "Tweet deleted successfully",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
         return res.status(500).json({
             message: "Internal server error",
             success: false
