4351ff6e225e965ca9d9dcd3aa6f34606a226d84
Abhijit694 <nayakabhijit543@gmail.com>
2026-02-03 12:45:22 +0530

    feat: route created for delete tweet functionality

--- backend/routes/tweet.routes.js
+++ backend/routes/tweet.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createTweet } from "../controllers/tweet.controller.js"
+import { createTweet, deleteTweet } from "../controllers/tweet.controller.js"
 import isAuthenticated from "../middlewares/auth.js"
 
 const router = express.Router()
 
 
 router.route("/create").post(isAuthenticated,createTweet)
+router.route("/delete/:id").delete(isAuthenticated,deleteTweet)
 
 
 export default router
