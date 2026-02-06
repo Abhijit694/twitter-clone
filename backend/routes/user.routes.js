2ec8326bf707b6bc6b24554c6659e4824eaa786e
Abhijit694 <nayakabhijit543@gmail.com>
2026-02-06 18:00:53 +0530

    feat : route created for bookmark tweet

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { login, logout, register } from "../controllers/user.controller.js"
+import { bookmarkTweet, login, logout, register } from "../controllers/user.controller.js"
+import isAuthenticated from "../middlewares/auth.js"
 
 const router = express.Router()
 
 router.route("/register").post(register)
 router.route("/login").post(login)
 router.route("/logout").get(logout)
+router.route("/bookmark/:id").put(isAuthenticated,bookmarkTweet)
 
 export default router
